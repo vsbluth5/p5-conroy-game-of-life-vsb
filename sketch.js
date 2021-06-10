646e15e694e140e18307e1555d91f9961650f96f
Glitch (hello-webpage) <none>
2021-06-10 01:42:45 +0000

    ✌️🛳 Checkpoint ./sketch.js:3856783/1628

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-let boardWith;
+let boardWidth;
 let boardHeight;
 let fallSpeed;
 let gravity
 let score;
 let gapSize;
 let gems;
+let prevX, prevY;
 
 function setup() {
   createCanvas(800, 600);
@@ -17,83 +18,21 @@ function draw() {
   // add scoring
   text("score: " + score, 50, 200);
 
-  ellipse(200, birdY, 50, 50);
-  birdY += fallSpeed;
-  fallSpeed += gravity;
-
-  // let the bird bounce off bottom
-  if (birdY + 25 > height) {
-    fallSpeed = -fallSpeed * 0.8;
-  }
-
-  // draw the moving pipe
-  // rect(pipePos, 0, 50, 250);
-  // rect(pipePos, 350, 50, height - 350);
-
-  rect(pipePos, 0, 50, gapHeight);
-  rect(pipePos, gapHeight + gapSize, 50, height - (gapHeight + gapSize));
-
-  pipePos += pipeSpeed;
-
-  if (pipePos < -50) {
-    score += 1;
-    pipePos = width;
-
-    // increase difficulty
-    pipeSpeed += -0.5;
-    gravity += 0.01;
-  }
-
-  // function takes seven paramters: rectX, rectY, rectWidth, rectHeight, circleX, circleY, circleDiameter
-  if (collideRectCircle(pipePos, 0, 50, gapHeight, 200, birdY, 50)) {
-    // score = 0
-    newGame();
-  }
-
-  if (
-    collideRectCircle(
-      pipePos,
-      350,
-      50,
-      height - gapHeight - gapSize,
-      200,
-      birdY,
-      50
-    )
-  ) {
-    // score = 0
-    newGame();
-  }
-
-  // if (collideRectCircle(pipePos, 0, 50, gapHeight, 200, birdY, 50)) {
-  //   newGame();
-  // }
-  // if (
-  //   collideRectCircle(
-  //     pipePos,
-  //     gapHeight + gapSize,
-  //     50,
-  //     height - (gapHeight + gapSize),
-  //     200,
-  //     birdY,
-  //     50
-  //   )
-  // ) {
-  //   newGame();
-  // }
 }
 
-function keyPressed() {
-  fallSpeed = -10;
+function mousePressed() {
+
 }
 
 function newGame() {
-  birdY = 300;
-  fallSpeed = -5;
-  gravity = 0.3;
-  pipePos = width;
-  pipeSpeed = -4;
-  score = 0;
-  gapSize = 200;
-  gapHeight = random(height - gapSize);
+  boardWidth = 8;
+  boardHeight = 8;
+
+}
+
+class Gem {
+  constructor(r, c, col){
+    this.row = r;
+    this.col = 
+  }
 }
