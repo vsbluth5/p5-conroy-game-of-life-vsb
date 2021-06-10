60f3c07d5178a7fd89f4e452dcb025fa5db7fa48
Glitch (hello-webpage) <none>
2021-06-10 14:08:24 +0000

    😂🍥 Checkpoint ./sketch.js:3856783/1577

--- sketch.js
+++ sketch.js
@@ -1,44 +1,18 @@
 let boardWidth;
 let boardHeight;
-let fallSpeed;
-let gravity
-let score;
-let jewels;
-let prevX, prevY;
-let blueImage, blueGem;
-let redImage, redGem;
-let yellowImage, yellowGem;
-let greenImage, greenGem;
-let purpleImage, purpleGem;
-let pinkImage, pinkGem;
-let orangeImage, orangeGem;
+let iterations;
+let cellImage;
+let adding;
 
 let gameBoard;
 
 function setup() {
-  createCanvas(410, 600);
+  createCanvas(windowWidth-10, windowHeight-1);
   colorMode(HSB, 360, 100, 100, 1);
-  boardWidth = 8;
-  boardHeight = 9;
-
-  blueImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fblue.png.png?v=1623289767088");
-  redImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fred.png.png?v=1623289770888")
-  yellowImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fyellow.png.png?v=1623289793357")
-  greenImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fgreen2.png?v=1623289735274")
-  purpleImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fpurple.png.png?v=1623289756175")
-  pinkImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fpink.png.png?v=1623289743473")
-  orangeImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Forange.png.png?v=1623289759358")
+  boardWidth = 100;
+  boardHeight = 100;
   
-  blueGem = {"source": blueImage, "color":"blue"}
-  redGem = {"source": redImage, "color":"red"}
-  yellowGem = {"source": yellowImage, "color":"yellow"}
-  greenGem = {"source": greenImage, "color":"green"}
-  purpleGem = {"source": purpleImage, "color":"purple"}
-  pinkGem = {"source": pinkImage, "color":"pink"}
-  orangeGem = {"source": orangeImage, "color":"orange"}
-  
-  
-  jewels = [blueGem, redGem, greenGem, purpleGem, pinkGem, yellowGem, orangeGem];
+  cellImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fgreen2.png?v=1623289735274")
   newGame();
 }
 
@@ -47,7 +21,7 @@ function draw() {
   // add scoring
   fill(220, 0, 100, 1)
   textSize(20)
-  text("score: " + score, 50, 520);
+  text("iteration: " + iterations, 50, 520);
   // Draw the logo at the new position.
   gameBoard.draw();
   
@@ -55,6 +29,10 @@ function draw() {
 }
 
 function mousePressed() {
+
+}
+
+function keyPressed() {
 
 }
 
