1dd363dfd9f103db171571abee16aa06ad12d8ee
Glitch (hello-webpage) <none>
2021-06-10 02:04:58 +0000

    ☕️🎣 Checkpoint ./sketch.js:3856783/551

--- sketch.js
+++ sketch.js
@@ -6,15 +6,16 @@ let score;
 let gapSize;
 let gems;
 let prevX, prevY;
+let blueImage, blueGem, redImage, redGem;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(400, 600);
   colorMode(HSL, 360)
   newGame();
 }
 
 function draw() {
-  background(220);
+  background(220, 90, 10);
   // add scoring
   text("score: " + score, 50, 200);
 
@@ -27,6 +28,9 @@ function mousePressed() {
 function newGame() {
   boardWidth = 8;
   boardHeight = 8;
+  blueImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fblue.png.png?v=1623289767088");
+  blueGem = {"sourcee":"https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fblue.png.png?v=1623289767088" ,"color":"blue"}
+  
 
 }
 
