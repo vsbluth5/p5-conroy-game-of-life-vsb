cfb0cc8568c29eba7aa2cb0ac19dba29bcaf8706
Glitch (hello-webpage) <none>
2021-06-10 03:38:44 +0000

    👗🍞 Checkpoint ./sketch.js:3856783/1679

--- sketch.js
+++ sketch.js
@@ -11,16 +11,34 @@ let gameBoard;;
 
 function setup() {
   createCanvas(400, 600);
-  colorMode(HSL, 360)
+  colorMode(HSL, 360);
+  boardWidth = 8;
+  boardHeight = 8;
+  blueImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fblue.png.png?v=1623289767088");
+  redImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fred.png.png?v=1623289770888")
+  yellowImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fyellow.png.png?v=1623289793357")
+  greenImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fgreen2.png?v=1623289735274")
+  purpleImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fpurple.png.png?v=1623289756175")
+  pinkImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fpink.png.png?v=1623289743473")
+  orangeImage = loadImage("https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Forange.png.png?v=1623289759358")
+  
+  
+  
+  
+  
+  blueGem = {"source": blueImage, "color":"blue"}
+  
+  
+  jewels = [blueGem];
   newGame();
 }
 
 function draw() {
   background(220, 90, 10);
   // add scoring
-  stroke(360, 0, 100)
+  stroke(0, 0, 100)
   textSize(16)
-  text("score: " + score, 50, 20);
+  text("score: " + score, 50, 500);
   // Draw the logo at the new position.
   gameBoard.draw();
   
@@ -32,12 +50,7 @@ function mousePressed() {
 }
 
 function newGame() {
-  boardWidth = 8;
-  boardHeight = 8;
-  blueGem = {"source": "https://cdn.glitch.com/5edd7c70-2d70-47e5-97ef-05e0c0718b7d%2Fblue.png.png?v=1623289767088", "color":"blue"}
   
-  blueImage = loadImage(blueGem.source);
-  jewels = [blueGem];
   gameBoard = new Board(boardWidth, boardHeight)
   
 }
